943c3af7b01bbd018af9b4ad84aafa4538c8b862
Vadim <dzida.vadim@yandex.ru>
2023-09-23 16:28:52 +0300

    Update project

---
+++ taskmanager/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+# отслеживаем переход на главную страницу
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+
+
+]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    """Выводит html текст на сервер"""
+    return render(request, 'main/index.html')
+
+
+
+def about(request):
+    return render(request, "main/about.html")
