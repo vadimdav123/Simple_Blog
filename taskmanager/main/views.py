8e4a90f88c1642eb4fe53efc9d1b46a34d5ce117
Vadim <dzida.vadim@yandex.ru>
2023-09-24 16:52:55 +0300

    UPDATE PROJECT

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 def index(request):
-    """Выводит html текст на сервер"""
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', "tasks": tasks})
 
 
 
 def about(request):
     return render(request, "main/about.html")
+
+def create(request):
+    error = ""
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Форма была неверной"
+
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error,
+
+    }
+    return render(request, "main/create.html", context)
